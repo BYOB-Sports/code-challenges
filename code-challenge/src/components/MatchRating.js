10f6e18b4e33d59c3d7c5a720c406cf463a6c53e
aniJani <rajkarnikarjanit@gmail.com>
2025-05-02 19:47:06 -0500

    fix: prevent showing previous success message before submitting again
    also remove usused imports

--- code-challenge/src/components/MatchRating.js
+++ code-challenge/src/components/MatchRating.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { storeRating, submitRating } from '../api/ratingApi';
+import { submitRating } from '../api/ratingApi';
 
 const MatchRating = ({ players, setPlayers }) => {
   const [selectedPlayer, setSelectedPlayer] = useState('');
@@ -16,7 +16,8 @@ const MatchRating = ({ players, setPlayers }) => {
     }
 
     setIsSubmitting(true);
-    setMessage('Please wait while we submit your rating...');
+
+    setMessage(`Submitting rating... This may take up to 10 seconds.`);
 
     try {
       const updatedPlayers = await submitRating(selectedPlayer, rating, players);
@@ -45,7 +46,10 @@ const MatchRating = ({ players, setPlayers }) => {
             <select
               id="player-select"
               value={selectedPlayer}
-              onChange={(e) => setSelectedPlayer(e.target.value)}
+              onChange={(e) => {
+                setSelectedPlayer(e.target.value);
+                setMessage('');
+              }}
               disabled={isSubmitting}
             >
               <option value="">-- Select a player --</option>
@@ -64,12 +68,15 @@ const MatchRating = ({ players, setPlayers }) => {
               max="7.0"
               step="0.1"
               value={rating}
-              onChange={(e) => setRating(parseFloat(e.target.value))}
+              onChange={(e) => {
+                setRating(parseFloat(e.target.value));
+                setMessage('');
+              }}
               disabled={isSubmitting}
             />
           </div>
 
-          <button type="submit" disabled={isSubmitting}>
+          <button type="submit" disabled={isSubmitting || !selectedPlayer}>
             {isSubmitting ? 'Submitting...' : 'Submit Rating'}
           </button>
 
@@ -79,6 +86,5 @@ const MatchRating = ({ players, setPlayers }) => {
     </>
   );
 };
-
 
 export default MatchRating;
